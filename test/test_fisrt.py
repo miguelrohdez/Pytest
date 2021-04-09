4627c93f6dcb3c7e102cbb56a50a94b70b9668f3
Miguel Alejandro Rojas Hernández <nomeacuerdox4@gmail.com>
2021-04-09 00:56:39 -0500

    Otras

---
+++ test/test_fisrt.py
@@ -0,0 +1,3 @@
+def test_hello_world():
+    hello = 'Hola mundo'
+    assert hello.upper()== 'HOLA MUNDO'
---
+++ test/test_fisrt_class.py
@@ -0,0 +1,4 @@
+class TestFirstClass:
+	def test_second_test(self):
+		hello = 'Hola mundo'
+		assert hello.upper()== 'HOLA MUNDO'
